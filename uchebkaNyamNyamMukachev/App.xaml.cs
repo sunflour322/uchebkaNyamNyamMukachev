9d8232423e0ed365f4047fced3c81216bd4cc7d3
202111 <202111@DESKTOP-HS0IL6L>
2024-03-30 14:20:29 +0300

    Добавьте файлы проекта.

---
+++ uchebkaNyamNyamMukachev/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using uchebkaNyamNyamMukachev.BD;
+
+
+namespace uchebkaNyamNyamMukachev
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static NyamNyam_Session2_MukachevEntities BD = new NyamNyam_Session2_MukachevEntities();
+    }
+}
---
+++ uchebkaNyamNyamMukachev/Pages/Recipe_for_DishesPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace uchebkaNyamNyamMukachev.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Recipe_for_DishesPage.xaml
+    /// </summary>
+    public partial class Recipe_for_DishesPage : Page
+    {
+        private int count = 0;
+        private int cost = 54;
+        private string CostText;
+        public Recipe_for_DishesPage()
+        {
+            InitializeComponent();
+
+            Name = "огурец";
+            NameDishTb.Text = $"Recipe for '{Name}'";
+            CategoryTb.Text = $"Category: {Name}";
+            CookTimeTb.Text = $"Cooking time: {Name}";
+            DescTb.Text = $"Short description: {Name}";
+            CostTb.Text = $"Total cost: {CostText}";
+        }
+
+        private void Button_Click_Plus(object sender, RoutedEventArgs e)
+        {
+            CountTb.Text = Convert.ToString(count + 1);
+            count++;
+            CostText = Convert.ToString(count * cost);
+            CostTb.Text = $"Total cost: {CostText}";
+        }
+
+        private void Button_Click_Minus(object sender, RoutedEventArgs e)
+        {
+            if (count > 0)
+            {
+                CountTb.Text = Convert.ToString(count - 1);
+                count--;
+                CostText = Convert.ToString(count * cost);
+                CostTb.Text = $"Total cost: {CostText}";
+            }
+        }
+    }
+}
